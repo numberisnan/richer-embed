9d8cf2dd2e97f9db957fde3e0443615100adda3a
farazkaleemmalik <43715001+xxxFarazxxx@users.noreply.github.com>
2019-07-14 19:44:57 +0300

    JSDoc and cleaning up code

--- index.js
+++ index.js
@@ -2,22 +2,31 @@ const {RichEmbed} = require("discord.js");
 const {basename} = require("path");
 
 class RicherEmbed extends RichEmbed {
-    constructor(channelObj, embedObj, options) {
+    /**
+     * @param {Channel} channelObj - Discord.js Channel instance for which the embed is being created
+     * @param {Object} options - Additional properties to add to embed. An existing embed can also be inserted to turn it into a RicherEmbed instance (may be buggy).
+     * @returns {RicherEmbed} Returns itself
+     */
+    constructor(channelObj, options) {
         super();
         this.channel = channelObj;
 
-        if (embedObj) {
-            for (var prop in embedObj) { //Merge in all embed object props
-                this[prop] = embedObj[prop];
+        if (options) {
+            for (var prop in options) { //Merge in all embed object props
+                if (options.hasOwnProperty(prop)) {
+                    this[prop] = options[prop];
+                }
+
             }
         }
-		
-		if (options) {
-            for (var prop in options) { //Merge in all options object props
-                this[prop] = options[prop];
-            }
-        }
+		return this;
     };
+
+    /**
+     * Personalises the embed for a User by changing the title and the thumbnail
+     * @param {User} authorObj - User to personalise
+     * @returns {RicherEmbed} Returns itself
+     */
     personalise(authorObj) {
         this
             .setThumbnail(authorObj.displayAvatarURL)
@@ -25,6 +34,11 @@ class RicherEmbed extends RichEmbed {
         return this;
     };
 
+    /**
+     * Attaches a local image as a thumbnail for the embed
+     * @param {String} pathName - Path to image. Relative paths are relative to entry file
+     * @returns {RicherEmbed} Returns itself
+     */
     setLocalThumbnail(pathName) {
         this
             .attachFiles([pathName])
@@ -32,6 +46,11 @@ class RicherEmbed extends RichEmbed {
         return this;
     };
 
+    /**
+     * Attaches a local image as the main image for the embed
+     * @param {String} pathName - Path to image. Relative paths are relative to entry file
+     * @returns {RicherEmbed} Returns itself
+     */
     setLocalImage(pathName) {
         this
             .attachFiles([pathName])
@@ -39,25 +58,46 @@ class RicherEmbed extends RichEmbed {
         return this;
     };
 
-    setContent(title, chat) {
+    /**
+     * Sets content for the embed
+     * @param {String} title - Title of embed
+     * @param {String} content - Content of embed, to be set embed.description
+     * @returns {RicherEmbed} Returns itself
+     */
+    setContent(title, content) {
         this
             .setTitle(title)
-            .setDescription(chat);
+            .setDescription(content);
         return this;
     };
 
+    /**
+     * Replaces a message with this embed
+     * @param {Message} messageToReplace - The message to replace. Note that this overrides the message channel previously set of the new provided is in a different channel
+     */
+
     replace(messageToReplace) {
-        return messageToReplace.channel.send({embed: this})
+        messageToReplace.channel.send({embed: this})
             .then(res => messageToReplace.delete(1000))
             .catch(console.log);
     };
 
+    /**
+     * Send the message to the channel in embed.channel
+     */
     send() {
-        return this.channel.send({embed: this});
+        this.channel.send({embed: this});
     }
 
-    chat(authorObj, command, url) {
-        this.personalise(authorObj).setImage(url).setDescription(command.split(" ").slice(1).join(" "));
+    /**
+     * Turns the embed into a chat embed, to be used by a bot command
+     * @param {User} authorObj - Author of chat command
+     * @param {String} command - Exact command in the form of "commandName parameters more_parameters". The first word is excluded from the embed's content.
+     * @param {String} imageUrl - Url of main image
+     * @returns {RicherEmbed} Returns itself
+     */
+    chat(authorObj, command, imageUrl) {
+        this.personalise(authorObj).setImage(imageUrl).setDescription(command.split(" ").slice(1).join(" ")); // Evaluates to a RichEmbed instance since setDescription is defined in parent class!
         return this;
     };
 }
